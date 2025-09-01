471be31a4f91afb37b6a155cc17dffc2502af59b
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-09-01 20:09:36 +0100

    fix: fix mysql db max connection issue

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -4,7 +4,11 @@ import * as authSchema from "./db/schemas/auth-schema";
 import * as userSchema from "./db/schemas/user-schema";
 import env from "./lib/env";
 
-const db = drizzle(env.DATABASE_URL, {
+declare global {
+  var _db: ReturnType<typeof drizzle> | undefined;
+}
+
+const connection = drizzle(env.DATABASE_URL, {
   schema: {
     ...authSchema,
     ...userSchema,
@@ -12,6 +16,12 @@ const db = drizzle(env.DATABASE_URL, {
   mode: "default",
   casing: "snake_case",
 });
+
+const db = globalThis._db || connection;
+
+if (env.NODE_ENV !== "production") {
+  globalThis._db = db;
+}
 
 export * from "drizzle-orm";
 export default db;
--- packages/db/src/lib/env.ts
+++ packages/db/src/lib/env.ts
@@ -1,6 +1,9 @@
 import z, { type ZodError } from "zod";
 
 const EnvSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
   DATABASE_URL: z.url(),
 });
 
