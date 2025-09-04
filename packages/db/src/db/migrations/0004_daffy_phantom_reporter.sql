e347e985e8206cd4e9ecebad14e6e58a2d0379db
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-09-04 15:58:00 +0100

    feat: perform migrations to the db

---
+++ packages/db/src/db/migrations/0004_daffy_phantom_reporter.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "product" ALTER COLUMN "sizes" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "product" ALTER COLUMN "colors" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "product" ALTER COLUMN "images" SET NOT NULL;
